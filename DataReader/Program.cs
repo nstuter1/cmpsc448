379f04e38f00d1e7f5a0e0f20e2d2d76888fcc54
Arcite <kristopher.wagner@outlook.com>
2014-02-03 18:38:28 -0500

    code to open file and ask for a feature number

--- DataReader/Program.cs
+++ DataReader/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+
 
 namespace DataReader
 {
@@ -10,7 +13,27 @@ namespace DataReader
     {
         static void Main(string[] args)
         {
-            // l2git kris
+            string filePath = Path.Combine(Environment.CurrentDirectory, @"trainingdata.csv");      // file that has the training data
+            int LINESIZE = 5250;                                                                    // how many characters to read from a line
+            string userInput;                                                                       // user supplied input to convert to feature number
+            int feature;                                                                            // which feature does the user want?
+
+            Console.WriteLine("Which feature would you like to analyze?");      // prompting user for input
+            userInput = Console.ReadLine();                                     // reading user input
+            feature = Convert.ToInt32(userInput);
+
+            Console.WriteLine(feature);
+
+            using (FileStream fs = File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.None))
+            {
+                byte[] b = new byte[LINESIZE];                  // buffer to read from the file
+                UTF8Encoding temp = new UTF8Encoding(true);     // I'm not 100% sure what this is
+
+                while( fs.Read(b, 0, b.Length) > 0 )            // reading a line
+                {
+                    Console.WriteLine(temp.GetString(b));       // printing the line read
+                }
+            }
         }
     }
 }
