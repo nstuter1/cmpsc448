fc19f33e542c15b2487eda8af49b232724fca21f
Arcite <kristopher.wagner@outlook.com>
2014-02-03 20:21:21 -0500

    attempted to parse data but failed

--- DataReader/Program.cs
+++ DataReader/Program.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-using System.Text;
-
 
 namespace DataReader
 {
@@ -15,8 +13,14 @@ namespace DataReader
         {
             string filePath = Path.Combine(Environment.CurrentDirectory, @"trainingdata.csv");      // file that has the training data
             int LINESIZE = 5250;                                                                    // how many characters to read from a line
+            int NUM_ENTRIES = 105472;                                                               // number of entries in the excel sheet
+            int NUM_FEATURES = 772;                                                                 // this number is the loss
             string userInput;                                                                       // user supplied input to convert to feature number
             int feature;                                                                            // which feature does the user want?
+            double[] data = new double[NUM_ENTRIES];                                                // array to hold the feature data
+            double[] loanLoss = new double[NUM_ENTRIES];                                            // parallel array to data that holds the loan loss information
+            double min = 0, max = 0;                                                                // min and max for the reange of the feature
+            string[] temporaryArray = new string[1];                                                // temporary variable for the line in the loop
 
             Console.WriteLine("Which feature would you like to analyze?");      // prompting user for input
             userInput = Console.ReadLine();                                     // reading user input
@@ -29,9 +33,9 @@ namespace DataReader
                 byte[] b = new byte[LINESIZE];                  // buffer to read from the file
                 UTF8Encoding temp = new UTF8Encoding(true);     // I'm not 100% sure what this is
 
-                while( fs.Read(b, 0, b.Length) > 0 )            // reading a line
+                while (fs.Read(b, 0, b.Length) > 0)            // reading a line
                 {
-                    Console.WriteLine(temp.GetString(b));       // printing the line read
+                    temporaryArray = System.Text.Encoding.UTF8.GetString(b).Split(',');
                 }
             }
         }
