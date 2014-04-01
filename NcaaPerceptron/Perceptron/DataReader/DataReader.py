9d5850bbc35eb9b6838b5ed5419d8c33ad677ed5
Rykkata <nstuter@hotmail.com>
2014-03-31 20:52:25 -0400

    No more overwriting the rows in the data reader

--- NcaaPerceptron/Perceptron/DataReader/DataReader.py
+++ NcaaPerceptron/Perceptron/DataReader/DataReader.py
@@ -22,12 +22,16 @@ for row in stats:
     season = row[0]
     team = int(row[1])
     score = float(row[2])
+
     if(score == 0.0):
-        np.savetxt("NCAAAppendStats.csv",row,delimiter=',', fmt="%s", header="Season,Team,Win%,OppWin%\n")  
-        print(row)
-        continue
+         if new_stats == None:
+             new_stats = row
+         else:
+             new_stats = np.concatenate((new_stats,row),axis=0)
+         print(row)
+         continue
+
     output_array = None
-    new_stats = None
     input_file = csv.DictReader(open("NCAARawData.csv"))
     for test_row in input_file:
         if(season == test_row["season"] and (team == int(test_row["wteam"]) or team == int(test_row["lteam"]))):
@@ -49,5 +53,7 @@ for row in stats:
         new_stats = output_row
     else:
         new_stats = np.concatenate((new_stats,output_row),axis=0)
-    np.savetxt("NCAAAppendStats.csv",new_stats,delimiter=',', fmt="%s", header="Season,Team,Win%,OppWin%\n")  
     print(row)
+
+np.savetxt("NCAAAppendStats.csv",new_stats,delimiter=',', fmt="%s", header="Season,Team,Win%,OppWin%\n")  
+
